dcc7dc7f12b7c1a5d1d5dcba2e80eb7e4ea3aa2d
zhangxinkun <zxk844977406@outlook.com>
2017-09-23 09:15:51 +0800

    不同层次的透明已经实现

--- app/src/main/java/com/zxk/appdial/MainActivity.java
+++ app/src/main/java/com/zxk/appdial/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zxk.appdial;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void click(View view) {
+
   }
 }
